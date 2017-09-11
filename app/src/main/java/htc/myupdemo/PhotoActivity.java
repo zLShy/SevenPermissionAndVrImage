5f23e453c30edabd9b4a26f4b166f0f74d6ecd0f
zhangli <zl123>
2017-09-11 17:28:36 +0800

    sec

---
+++ app/src/main/java/htc/myupdemo/PhotoActivity.java
@@ -0,0 +1,18 @@
+package htc.myupdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+
+public class PhotoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_photo);
+    }
+}
